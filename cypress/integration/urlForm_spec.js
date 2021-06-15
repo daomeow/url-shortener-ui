cdb9e0b0daada0ed34ca266de7c72fef67537b79
Melanie Daoheuang <daoh0031@gmail.com>
2021-06-15 11:05:59 -0600

    Add command for Cypress

---
+++ cypress/integration/urlForm_spec.js
@@ -0,0 +1,13 @@
+// When a user visits the page, they can view the page title and the existing shortened URLs
+// When a user visits the page, they can view the Form with the proper inputs
+// When a user fills out the form, the information is reflected in the input fields
+
+describe('UrlForm', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('should display the title and existing URLs', () => {
+    
+  })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,1 +1,8 @@
+const baseUrl = 'http://localhost:3001/api/v1/urls';
 
+Cypress.Commands.add('interceptUrl', () => {
+  cy.fixture('../fixtures/url.json')
+    .then((json) => {
+      cy.intercept('GET', baseUrl, json)
+    })
+})
